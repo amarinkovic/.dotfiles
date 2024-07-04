b35b77b349f6ab43319d4dc9179ccb0ca301e51f
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2024-07-04 22:47:57 +0200

    feat: migrate dotfiles to GNU Stow layout
    thanks typecraft!

---
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,60 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "tsserver", "solidity", "rust_analyzer", "metals" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+
+      lspconfig.metals.setup({})
+
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.solidity.setup({
+				capabilities = capabilities,
+				settings = {
+					solidity = {
+						includePath = "",
+						-- remapping = { ["@OpenZeppelin/"] = 'OpenZeppelin/openzeppelin-contracts@4.6.0/' },
+						allowPaths = {},
+					},
+				},
+			})
+
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+				settings = {
+					["rust-analyzer"] = {
+						diagnostics = {
+							enable = false,
+						},
+					},
+				},
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			-- vim.keymap.set('n', 'gd', vim.lsp.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
