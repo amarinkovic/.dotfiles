f629b002c8d82747118bd56a8d03b9c646ad4f0e
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2024-11-30 17:31:34 +0100

    chore(nvim): purge completion config

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -3,31 +3,11 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 	},
 	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	--  {
-	--    "supermaven-inc/supermaven-nvim",
-	--    config = function()
-	--      require("supermaven-nvim").setup({})
-	--    end,
-	--  },
-	{
 		"hrsh7th/nvim-cmp",
 		config = function()
-			-- Set up nvim-cmp.
 			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
 				window = {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
@@ -41,8 +21,6 @@ return {
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
-					-- { name = "luasnip" }, -- For luasnip users.
-					-- { name = "supermaven" },
 				}, {
 					{ name = "buffer" },
 				}),
