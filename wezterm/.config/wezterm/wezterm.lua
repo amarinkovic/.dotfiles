8a585116e2498abb3f1243fd5d9039bf02b20af0
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2024-12-18 12:22:59 +0100

    feat(wezterm): set font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,3 +1,12 @@
-return {
-  color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
-}
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.color_scheme = "Catppuccin Mocha" -- or Macchiato, Frappe, Latte
+
+config.font = wezterm.font('JetBrains Mono')
+-- config.font = wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true })
+
+return config
