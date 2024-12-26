ea5f5425d65291e8f6fa5562099c759729c7ac1d
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2024-12-26 22:30:00 +0100

    fix(nvim): update cord config

--- nvim/.config/nvim/lua/plugins/cord.lua
+++ nvim/.config/nvim/lua/plugins/cord.lua
@@ -1,30 +1,21 @@
 return {
-	"vyfor/cord.nvim",
-	branch = "client-server",
-	build = ":Cord fetch",
-	config = function()
-		require("cord").setup({
-			editor = {
-				client = "neovim",
-			},
-			display = {
-				theme = "pastel", -- onyx or pastel
-			},
-			idle = {
-				enabled = true,
-				timeout = 300000,
-				details = "Thinking 🤔",
-			},
-			buttons = {
-				{
-					label = "View Repository",
-					url = function(opts)
-						if not opts.is_idle then
-							return opts.repo_url
-						end
-					end,
-				},
-			},
-		})
-	end,
+  "vyfor/cord.nvim",
+  branch = "client-server",
+  build = ":Cord fetch",
+  config = function()
+    require("cord").setup({
+      editor = {
+        client = "neovim",
+        tooltip = "Neovim The Superior Text Editor",
+      },
+      display = {
+        theme = "pastel", -- onyx or pastel
+      },
+      idle = {
+        enabled = true,
+        timeout = 300000,
+        details = "Thinking 🤔",
+      },
+    })
+  end,
 }
