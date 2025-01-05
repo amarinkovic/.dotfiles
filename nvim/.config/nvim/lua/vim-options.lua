434471dabe538d443b7a18e55194fff680a94722
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-01-05 11:52:15 +0100

    feat(nvim): add cellular automation

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -19,8 +19,6 @@ vim.opt.updatetime = 300
 vim.opt.termguicolors = true
 
 -- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
