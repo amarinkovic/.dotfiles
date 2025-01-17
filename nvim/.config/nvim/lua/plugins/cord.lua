d67656edb5d8f15917d1574fe04427d8b8513f3e
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-01-17 16:02:49 +0100

    feat(nvim): cord buttons and custom file type config

--- nvim/.config/nvim/lua/plugins/cord.lua
+++ nvim/.config/nvim/lua/plugins/cord.lua
@@ -2,29 +2,38 @@ return {
   "vyfor/cord.nvim",
   build = "./build || .\\build",
   event = "VeryLazy",
-  opts = {
-    usercmds = true,
-    editor = {
-      client = "neovim",
-      tooltip = "Neovim The Superior Text Editor",
-    },
-    display = {
-      theme = "pastel",    -- onyx or pastel
-      show_repository = true, -- Display 'View repository' button linked to repository url, if any
-    },
-    lsp = {
-      show_problem_count = true, -- Display number of diagnostics problems
-      severity = 1,           -- 1 = Error, 2 = Warning, 3 = Info, 4 = Hint
-      scope = "workspace",    -- buffer or workspace
-    },
-    idle = {
-      enabled = true,
-      timeout = 300000,
-      details = "Thinking 🤔",
-    },
-    buttons = {
-      { label = "View Repository", url = "git" },
-      { label = "My Github",       url = "https://github.com/amarinkovic" },
-    },
-  },
+  opts = {},
+  config = function()
+    require("cord").setup({
+      usercmds = true,
+      editor = {
+        client = "neovim",
+        tooltip = "The Superior Text Editor",
+      },
+      display = {
+        theme = "pastel", -- onyx or pastel
+        show_repository = false,
+      },
+      lsp = {
+        show_problem_count = true, -- Display number of diagnostics problems
+        severity = 1, -- 1 = Error, 2 = Warning, 3 = Info, 4 = Hint
+        scope = "workspace", -- buffer or workspace
+      },
+      idle = {
+        enabled = true,
+        timeout = 300000,
+        details = "Thinking 🤔",
+      },
+      buttons = {
+        { label = "Github", url = "https://github.com/amarinkovic" },
+      },
+      assets = {
+        solidity = {
+          -- icon = "https://images.seeklogo.com/logo-png/42/1/solidity-logo-png_seeklogo-426731.png?v=1957664661399450704",
+          tooltip = "Solidity Smart Contract",
+          -- text = "Writing in Solidity",
+        },
+      },
+    })
+  end,
 }
