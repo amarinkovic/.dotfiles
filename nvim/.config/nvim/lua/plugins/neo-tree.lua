754e78a0fb4634930d65eb66be8bebf25b0a99f0
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-01-17 13:59:11 +0100

    feat(nvim): better default neo-tree filtering

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,20 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_by_name = {
+            ".git",
+            ".DS_Store",
+          },
+          always_show = {
+            ".env",
+          },
+        },
+      },
+    })
     vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", { desc = "Reveal neotree" })
   end,
 }
