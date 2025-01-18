b1024cef7b17c9ff21d85ab43b572e01a8f3d852
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-01-18 22:05:58 +0100

    fix(nvim): cellular automaton keybind

--- nvim/.config/nvim/lua/plugins/cellular-automation.lua
+++ nvim/.config/nvim/lua/plugins/cellular-automation.lua
@@ -1,6 +1,6 @@
 return {
   "eandrju/cellular-automaton.nvim",
   config = function()
-    vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomation make_it_rain<CR>")
+    vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
   end,
 }
