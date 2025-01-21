fb62d1b46744c023203d8f1241c6e58f37005fe3
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-01-21 17:58:29 +0100

    fix(nvim): simplify java config

--- nvim/.config/nvim/lua/plugins/nvim-java.lua
+++ nvim/.config/nvim/lua/plugins/nvim-java.lua
@@ -2,22 +2,6 @@ return {
   "nvim-java/nvim-java",
   config = false,
   dependencies = {
-    {
-      "neovim/nvim-lspconfig",
-      opts = {
-        servers = {
-          jdtls = {
-            -- Your custom jdtls settings goes here
-          },
-        },
-        setup = {
-          jdtls = function()
-            require("java").setup({
-              -- Your custom nvim-java configuration goes here
-            })
-          end,
-        },
-      },
-    },
+    { "neovim/nvim-lspconfig" },
   },
 }
