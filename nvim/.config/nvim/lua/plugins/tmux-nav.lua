53702a65036e477c20b0e24240bfdfa10d4949b7
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-02-03 22:03:13 +0100

    feat: seamless tmux/vim pane navigation

---
+++ nvim/.config/nvim/lua/plugins/tmux-nav.lua
@@ -0,0 +1,18 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateDown",
+    "TmuxNavigateRight",
+    "TmuxNavigateProcessList",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-\\", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+}
