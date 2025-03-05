1f8b103e426c9385163e81e7f3210b66a9c1b73a
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-03-05 09:01:10 +0100

    chore(nvim): add rust logo for cord presence

--- nvim/.config/nvim/lua/plugins/cord.lua
+++ nvim/.config/nvim/lua/plugins/cord.lua
@@ -14,13 +14,13 @@ return {
         tooltip = "The Superior Text Editor",
       },
       display = {
-        theme = "onyx", -- onyx or pastel
+        theme = "onyx",             -- onyx or pastel
         show_repository = false,
       },
       lsp = {
-        show_problem_count = true, -- Display number of diagnostics problems
-        severity = 1, -- 1 = Error, 2 = Warning, 3 = Info, 4 = Hint
-        scope = "workspace", -- buffer or workspace
+        show_problem_count = true,  -- Display number of diagnostics problems
+        severity = 1,               -- 1 = Error, 2 = Warning, 3 = Info, 4 = Hint
+        scope = "workspace",        -- buffer or workspace
       },
       idle = {
         enabled = true,
@@ -40,6 +40,10 @@ return {
           icon = "https://programming.dev/pictrs/image/d9362f3f-d7a2-4837-8fef-47bc92ccc91e.png",
           tooltip = "Zig",
           -- text = "Writing Zig",
+        },
+        rust = {
+          icon = "https://www.techzine.eu/wp-content/uploads/2024/11/Rust_programming_language_black_logo.svg_.jpg",
+          tooltip = "Rust",
         },
       },
     })
