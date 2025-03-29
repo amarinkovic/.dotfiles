faaf890aa3d2c40ab5827d7cbeffd1e8e994e88e
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-03-29 20:36:50 +0100

    fix(nvim): formatters by file type

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -5,10 +5,11 @@ return {
     require("conform").setup({
       formatters_by_ft = {
         lua = { "stylua" },
-        -- python = { "isort", "black" },
         rust = { "rustfmt", lsp_format = "fallback" },
-        javascript = { "prettierd", "prettier", stop_after_first = true },
         solidity = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        python = { "isort", "black" },
       },
     })
 
