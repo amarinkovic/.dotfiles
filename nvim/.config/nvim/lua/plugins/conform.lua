94457c3f6325187b5772836c1cf36812001bfd94
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-04-13 19:18:19 +0200

    feat(nvim): gradle lsp, make formatter

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -13,6 +13,8 @@ return {
         typescriptreact = { "prettierd", "prettier", stop_after_first = true },
         python = { "isort", "black" },
         java = { "checkstyle", lsp_format = "fallback" },
+        groovy = { "npm-groovy-lint", lsp_format = "fallback" },
+        make = { "checkmake", lsp_format = "fallback" },
       },
     })
 
