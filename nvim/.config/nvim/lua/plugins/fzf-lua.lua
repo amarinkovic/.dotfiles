d71884cd29bb7de660921e3bcc66fc588fc499fc
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-04-18 21:19:05 +0200

    feat(nvim): remove telescope, cleanup fzf-lua config

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -3,12 +3,22 @@ return {
   event = "VeryLazy",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
+    local actions = require("fzf-lua.actions")
+
+    require("fzf-lua").setup({
+      actions = {
+        files = { ["default"] = actions.file_edit },
+        grep = { ["default"] = actions.file_edit },
+        buffers = { ["default"] = actions.buf_edit }
+      }
+    })
+
     local fzf = require("fzf-lua")
-    vim.keymap.set("n", "<C-p>", function() fzf.files() end, { desc = "[FZF] Find files" })
-    vim.keymap.set("n", "<leader>fg", function() fzf.live_grep() end, { desc = "[FZF] Live Grep" })
-    vim.keymap.set("n", "<leader>fb", function() fzf.buffers() end, { desc = "[FZF] Find buffers" })
-    vim.keymap.set("n", "<leader>fr", function() fzf.lsp_references() end, { desc = "[FZF] LSP References" })
-    vim.keymap.set("n", "<leader>fk", function() fzf.keymaps() end, { desc = "[FZF] Find keymaps" })
-    vim.keymap.set("n", "<leader>fh", function() fzf.help_tags() end, { desc = "[FZF] Find help tags" })
+    vim.keymap.set("n", "<C-p>", function() fzf.files() end, { desc = "Find files" })
+    vim.keymap.set("n", "<leader>fg", function() fzf.live_grep() end, { desc = "Live Grep" })
+    vim.keymap.set("n", "<leader>fb", function() fzf.buffers() end, { desc = "Find buffers" })
+    vim.keymap.set("n", "<leader>fr", function() fzf.lsp_references() end, { desc = "LSP References" })
+    vim.keymap.set("n", "<leader>fk", function() fzf.keymaps() end, { desc = "Find keymaps" })
+    vim.keymap.set("n", "<leader>fh", function() fzf.help_tags() end, { desc = "Find help tags" })
   end,
 }
