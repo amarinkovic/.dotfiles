aa6bfac7d3614df830bdfd44fd08dd2872b0540b
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-06-09 23:00:44 +0200

    feat(waybar): add fancy css
    thanks @acarl005, see here:
    https://github.com/acarl005/dotfiles/tree/master/waybar

--- waybar/.config/waybar/player.sh
+++ waybar/.config/waybar/player.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 player_status=$(playerctl status -p spotify)
 if [ "$player_status" = "Playing" ]; then
-    echo "  $(playerctl metadata artist -p spotify) - $(playerctl metadata title -p spotify) "
+    echo "   $(playerctl metadata artist -p spotify) - $(playerctl metadata title -p spotify) "
 elif [ "$player_status" = "Paused" ]; then
-    echo "  $(playerctl metadata artist -p spotify) - $(playerctl metadata title -p spotify) "
+    echo "   $(playerctl metadata artist -p spotify) - $(playerctl metadata title -p spotify) "
 fi
 
