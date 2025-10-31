7951f22dbfab44af60917680bcac273d51828ede
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2025-10-31 15:28:22 +0100

    feat(nvim): show attached LSPs in lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -12,6 +12,26 @@ return {
             path = 1, -- relative path
           },
         },
+        lualine_x = {
+          {
+            function()
+              local clients = vim.lsp.get_active_clients()
+              if #clients > 0 then
+                local client_names = {}
+                for _, client in ipairs(clients) do
+                  table.insert(client_names, client.name)
+                end
+                return "LSP: " .. table.concat(client_names, ", ")
+              end
+              return ""
+            end,
+            icon = " ",
+            color = { fg = "#ffffff" },
+          },
+          "encoding",
+          "fileformat",
+          "filetype",
+        },
       },
     })
   end,
