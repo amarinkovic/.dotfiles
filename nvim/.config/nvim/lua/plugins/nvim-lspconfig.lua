1c76ce8def900c1a33cdadef929dab1834ed974c
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2026-03-14 20:19:58 +0100

    fix(nvim): centralize blink capabilities accross all LSPs

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -6,5 +6,12 @@ return {
       { "williamboman/mason.nvim", config = true, version = "^2.0.0", opts = { ui = { border = "rounded" } } },
       { "saghen/blink.cmp" },
     },
+    config = function()
+      -- Centralize blink.cmp capabilities so ALL servers (including those
+      -- started via vim.lsp.enable() in lsp/) inherit them automatically.
+      vim.lsp.config("*", {
+        capabilities = require("blink.cmp").get_lsp_capabilities(),
+      })
+    end,
   },
 }
