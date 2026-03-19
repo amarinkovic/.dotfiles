089f4a3a1555b5c95309c9bcee6f2bd5478d390f
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2026-03-19 14:30:28 +0100

    fix(nvim): copilot ghost text & better tab with blink

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,31 +1,11 @@
 return {
   "zbirenbaum/copilot.lua",
   event = "VeryLazy",
-  config = function()
-    require("copilot").setup({
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-      },
-      panel = { enabled = false },
-      filetypes = {
-        ["*"] = true,
-      },
-    })
-
-    vim.api.nvim_create_autocmd("FileType", {
-      group = vim.api.nvim_create_augroup("copilot", { clear = true }),
-      pattern = "*",
-      callback = function()
-        local suggestion = require("copilot.suggestion")
-        vim.keymap.set("i", "<Tab>", function()
-          if suggestion.is_visible() then
-            suggestion.accept()
-          else
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true))
-          end
-        end, { remap = true })
-      end,
-    })
-  end,
+  opts = {
+    suggestion = { enabled = false },
+    panel = { enabled = false },
+    filetypes = {
+      ["*"] = true,
+    },
+  },
 }
