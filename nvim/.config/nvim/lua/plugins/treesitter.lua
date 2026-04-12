bf0cab6bc27f6e4d083c9f53ecda81cf73ee0bc5
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2026-04-12 18:14:34 +0200

    fix(nvim): treesitter config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile" },
+  lazy = false,
   init = function()
     vim.filetype.add({
       pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
@@ -12,10 +13,48 @@ return {
     vim.treesitter.language.register("markdown", "mdx")
   end,
   config = function()
-    require("nvim-treesitter.configs").setup({
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
+    require("nvim-treesitter").setup({
+      install_dir = vim.fn.stdpath("data") .. "/site",
+    })
+
+    require("nvim-treesitter").install({
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "markdown",
+      "markdown_inline",
+      "bash",
+      "json",
+      "yaml",
+      "toml",
+      "javascript",
+      "typescript",
+      "tsx",
+      "html",
+      "css",
+      "python",
+      "go",
+      "rust",
+      "c",
+      "hyprlang",
+      "solidity",
+      "zig",
+      "glsl",
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(args)
+        local lang = vim.treesitter.language.get_lang(args.match) or args.match
+        local ok = pcall(vim.treesitter.language.inspect, lang)
+        if ok then
+          vim.treesitter.start()
+          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          vim.wo[0][0].foldmethod = "expr"
+          vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+          vim.wo[0][0].foldlevel = 99
+        end
+      end,
     })
   end,
 }
