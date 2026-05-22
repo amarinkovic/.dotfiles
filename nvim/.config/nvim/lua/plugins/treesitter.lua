64db8a90caefbbc9c01985cf728ff746034c9ab8
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2026-05-22 11:33:27 +0200

    fix(nvim): move to tree-sitter-manager

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,5 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "main",
-  build = ":TSUpdate",
+  "romus204/tree-sitter-manager.nvim",
   lazy = false,
   init = function()
     vim.filetype.add({
@@ -13,43 +11,50 @@ return {
     vim.treesitter.language.register("markdown", "mdx")
   end,
   config = function()
-    require("nvim-treesitter").setup({
-      install_dir = vim.fn.stdpath("data") .. "/site",
-    })
-
-    require("nvim-treesitter").install({
-      "lua",
-      "vim",
-      "vimdoc",
-      "query",
-      "markdown",
-      "markdown_inline",
-      "bash",
-      "json",
-      "yaml",
-      "toml",
-      "javascript",
-      "typescript",
-      "tsx",
-      "html",
-      "css",
-      "python",
-      "go",
-      "rust",
-      "c",
-      "hyprlang",
-      "solidity",
-      "zig",
-      "glsl",
+    require("tree-sitter-manager").setup({
+      auto_install = true,
+      highlight = true,
+      ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "markdown",
+        "markdown_inline",
+        "bash",
+        "json",
+        "yaml",
+        "toml",
+        "javascript",
+        "typescript",
+        "tsx",
+        "html",
+        "css",
+        "python",
+        "go",
+        "rust",
+        "c",
+        "hyprlang",
+        "solidity",
+        "zig",
+        "glsl",
+        "scala",
+        "gitcommit",
+        "gitignore",
+        "git_rebase",
+        "gitattributes",
+        "diff",
+        "regex",
+        "comment",
+        "jsdoc",
+        "printf",
+      },
     })
 
     vim.api.nvim_create_autocmd("FileType", {
       callback = function(args)
         local lang = vim.treesitter.language.get_lang(args.match) or args.match
-        local ok = pcall(vim.treesitter.language.inspect, lang)
-        if ok then
-          vim.treesitter.start()
-          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        if pcall(vim.treesitter.language.inspect, lang) then
           vim.wo[0][0].foldmethod = "expr"
           vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
           vim.wo[0][0].foldlevel = 99
