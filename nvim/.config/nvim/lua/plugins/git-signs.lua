aa284d758db0592063d2b9148c26e2e96827ef29
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2026-07-01 15:07:09 +0200

    fix(nvim): gitsigns virtual text config

--- nvim/.config/nvim/lua/plugins/git-signs.lua
+++ nvim/.config/nvim/lua/plugins/git-signs.lua
@@ -8,6 +8,8 @@ return {
   },
   opts = {
     current_line_blame = true,
+    current_line_blame_opts = { delay = 100, },
+    current_line_blame_formatter = '     <author>, <author_time:%R> - <summary> ',
     signs = {
       add = { text = "+" },
       change = { text = "~" },
