9d1d0fa9db81207e382a643d9fecc1bc3b4664cf
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2026-07-01 14:54:47 +0200

    feat(nvim): keybinds for gitsigns

--- nvim/.config/nvim/lua/plugins/git-signs.lua
+++ nvim/.config/nvim/lua/plugins/git-signs.lua
@@ -1,7 +1,13 @@
 return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPost", "BufNewFile" },
+  keys = {
+    { "<leader>gb", "<cmd>Gitsigns blame<cr>", desc = "Gitsigns blame" },
+    { "<leader>gl", "<cmd>Gitsigns blame_line<cr>", desc = "Gitsigns blame line" },
+    { "<leader>gd", "<cmd>Gitsigns diffthis<cr>", desc = "Gitsigns diffthis" },
+  },
   opts = {
+    current_line_blame = true,
     signs = {
       add = { text = "+" },
       change = { text = "~" },
