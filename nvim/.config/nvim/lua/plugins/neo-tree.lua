aea3d58de9fedf3882f539513bbe3cc92ac8bd81
Aleksandar Marinkovic <alexandar.marinkovic@gmail.com>
2026-07-14 11:09:50 +0200

    chore(nvim): don't show hidden files by default

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
     require("neo-tree").setup({
       filesystem = {
         filtered_items = {
-          hide_dotfiles = false,
+          hide_dotfiles = true,
           hide_by_name = {
             ".git",
             ".DS_Store",
